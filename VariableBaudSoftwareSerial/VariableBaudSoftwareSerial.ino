068b259a8a0a41ea78639a9a19230c3639ede760
Kanyon Edvall <kedvall123@gmail.com>
2017-09-30 11:50:56 -0500

    Added variable baud rate software serial sketch

---
+++ VariableBaudSoftwareSerial/VariableBaudSoftwareSerial.ino
@@ -0,0 +1,44 @@
+/*
+  Software serial multple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+long baud_rate;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.println("Arduino attached!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    baud_rate = Serial.parseInt();
+    Serial.print("Setting baud rate to ");
+    Serial.println(baud_rate);
+
+    mySerial.end();
+    mySerial.begin(baud_rate);
+  }
+}
