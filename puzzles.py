c7656fbe44f45ab2bc9ad1453d9e08269b133f53
Shayna Kapadia <shaynak2@illinois.edu>
2019-05-10 19:54:10 -0500

    two puzzles down

--- puzzles.py
+++ puzzles.py
@@ -2,24 +2,45 @@
 def puzzleOne():
     answer = ""
     while(answer != "g" or answer != "G"):
-        answer = input("What always ends everything?")
+        answer = input("What always ends everything?\n")
         if(answer == "g" or answer == "G"):
-            print("You got it!")
+            print("You got it!\n")
             return 1;
         else:
-            print("That's not right you dum dum!")
-            value = input("Do you give up? (Y/N)")
+            print("That's not right you dum dum!\n")
+            value = input("Do you give up? (Y/N)\n")
             if(value == "Y"):
-                print("HAHAHA YOU LOSE")
+                print("HAHAHA YOU LOSE\n")
                 return 0;
             else:
-                print("Alright, lets try again.")
-
+                print("Alright, lets try again.\n")
     return 0;
 
+def puzzleTwo():
+    answer = ""
+    while(answer != "g" or answer != "G"):
+        answer = input("I can fly, but I don't have wings. I don't have eyes, but I can cry! What I am?\n")
+        if(answer == "A cloud" or answer == "a cloud" or answer == "cloud" or answer == "Cloud"  or answer == "A Cloud"  or answer == "a Cloud"):
+            print("That's right, one more and you can have your files back!\n")
+            return 1;
+        else:
+            print("Big oof, incorrect my dude!")
+            value = input("Do you give up? (Y/N)\n")
+            if(value == "Y"):
+                print("HAHAHA YOU LOSE\n")
+                return 0;
+            else:
+                print("Have another go!\n")
+    return 0;
 
+# Call encryption function here
+# call key encryption function here
+num = 0;
 print("You've been HACKED! \nSoRrY bUt AlL yOuR fIlEs HaVe bEeN eNcRyPtEd. \nIf YoU wAnT tHeM bAcK, sOlVe thEsE pUzZleS. OThErwIse, wE'lL delEte EveRytHinG!")
-if(puzzleOne() == 1):
-    print("Your files have been decrypted, enjoy your day!")
+num = num + puzzleOne()
+num = num + puzzleTwo()
+if(num == 2):
+    # decrypt files here
+    print("Your files have been decrypted, enjoy your day!\n")
 else:
-    print("Sorry, all your files are gone.")
+    print("Sorry, all your files are gone.\n")
