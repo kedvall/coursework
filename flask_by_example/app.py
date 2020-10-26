d79226ce09df2d08f3c9692e265ea00c01e2ef1e
Kanyon Edvall <kanyon@life-foundry.com>
2020-10-26 00:35:26 -0500

    Add basic config

--- flask_by_example/app.py
+++ flask_by_example/app.py
@@ -1,7 +1,17 @@
+import os
 from flask import Flask
-flask_app = Flask(__name__)
 
+
+flask_app = Flask(__name__)
 print("Name:", __name__)
+
+flask_config = os.getenv('APP_SETTINGS')
+if flask_config is None:
+    print("- Warning: No APP_SETTING env var found, defaulting to config.DevelopmentConfig")
+    flask_config = 'config.DevelopmentConfig'
+print("Config:", flask_config)
+
+flask_app.config.from_object(flask_config)
 
 @flask_app.route('/')
 def hello():
